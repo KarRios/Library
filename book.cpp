007a0b3f2a2f1c4258cb1fceadb92c1802021c89
KarRios <43351714+KarRios@users.noreply.github.com>
2019-04-16 16:49:46 -0700

    Add files via upload

---
+++ book.cpp
@@ -0,0 +1,34 @@
+#include "book.h"
+
+Book::Book(int id, string bookName, string auth, string cat) {
+  bookID = id;
+  title = bookName;
+  author = auth;
+  category = cat;
+}
+
+string Book::getTitle() {
+    return title; // complete
+}
+
+string Book::getAuthor() {
+    return author; // complete
+}
+
+string Book::getCategory() {
+    return category; // complete
+}
+
+int Book::getId() {
+    return bookID; // complete
+}
+
+void Book::setPersonPtr(Person * ptr) {
+  personPtr = ptr;
+  delete ptr;
+  ptr = nullptr;
+} // complete
+
+Person * Book::getPersonPtr() {
+    return personPtr; // complete
+}
